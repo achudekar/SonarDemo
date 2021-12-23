55fc537d67ea4cbdc19ae8e7485cd90fb405ef69
Rishab Shah <rishabshah@Rishabs-MacBook-Pro.local>
2021-12-23 13:28:33 +0530

    added hi

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello,I guess you are in main file");
+		System.out.println("Hi");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
