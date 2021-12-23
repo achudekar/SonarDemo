cfd60ecb37cb18e254a029ead4b7fdd04c0bd77f
achudekar <90025867+achudekar@users.noreply.github.com>
2021-12-23 13:14:32 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello, you are in main file");
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
